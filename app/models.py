ae4b26a1f6d6302800fc725d536a1688c89dcc00
andrewre23 <andrewre@umich.edu>
2017-02-15 21:37:41 -0800

    added portfolio and holding models

--- app/models.py
+++ app/models.py
@@ -1,5 +1,23 @@
 from . import db
 
+class Portfolio(db.Model):
+    # model for base portfolio object
+    __tablename__ = 'portfolios'
+    id = db.Column(db.Integer,primary_key=True,autoincrement=True)
+    name = db.Column(db.String(25),unique=True)
+    cash = db.Column(db.Float)
+    holdings = db.relationship('Holding',backref='portfolio',lazy='dynamic')
+
+class Holding(db.Model):
+    # model for holdings associated with portfolios
+    __tablename__ = 'holdings'
+    id = db.Column(db.Integer,primary_key=True,autoincrement=True)
+    symbol = db.Column(db.String(6))
+    shares = db.Column(db.Integer)
+    purch_date = db.Column(db.Date)
+    purch_price = db.Column(db.Numeric)
+    portfolio_id = db.Column(db.Integer,db.ForeignKey('portfolios.id'))
+
 
 class Ticker_Dataset(db.Model):
     # model definition for HDF5 ticker data storage
