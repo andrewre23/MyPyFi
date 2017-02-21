0e8a9870d647cbe0e39c708dc2453e8138d9ca9d
andrewre23 <andrewre@umich.edu>
2017-02-20 18:34:37 -0800

    changed functions to be tied to models, rather than free-floating import functions

--- app/models.py
+++ app/models.py
@@ -5,13 +5,13 @@ class Portfolio(db.Model):
     # model for base portfolio object
     __tablename__ = 'portfolios'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(25), unique=True)
-    cash = db.Column(db.Float)
-    market_value = db.Column(db.Float)
-    total_profit = db.Column(db.Float)
-    invested = db.Column(db.Float)
-    profit_percent = db.Column(db.Float)
-    num_holdings = db.Column(db.Integer)
+    name = db.Column(db.String(25), unique=True)    # name of portfolio
+    cash = db.Column(db.Float)                      # cash holdings within portfolio
+    market_value = db.Column(db.Float)              # market value of all holdings + cash
+    total_profit = db.Column(db.Float)              # total profits of all holdings
+    invested = db.Column(db.Float)                  # total amount invested into holdings - excludes cash holding
+    profit_percent = db.Column(db.Float)            # percent return on current amount invested - excludes cash holding
+    num_holdings = db.Column(db.Integer)            # total number of holdings in portfolio - excludes cash holding
     holdings = db.relationship('Holding', backref='portfolio', lazy='dynamic')
 
     def __init__(self, name, cash):
@@ -25,10 +25,12 @@ class Portfolio(db.Model):
         return '<Name %r>' % self.name
 
     def update_last_price(self):
+        # update last_price for all holdings in portfolio
         for holding in self.holdings:
             holding.update()
 
     def update_market_value(self):
+        # update market_value and invested attributes of portfolio
         self.market_value = 0
         self.invested = 0
         self.market_value += self.cash
@@ -39,6 +41,7 @@ class Portfolio(db.Model):
         db.session.add(self)
 
     def update_profit(self):
+        # update profit attributes
         self.total_profit = self.market_value - self.invested - self.cash
         if self.invested:
             self.profit_percent = round(self.total_profit / self.invested, 4)
@@ -47,10 +50,12 @@ class Portfolio(db.Model):
         db.session.add(self)
 
     def update_holding_count(self):
+        # update number of holdings currently held
         self.num_holdings = Holding.query.filter_by(portfolio_id=self.id).count()
         db.session.add(self)
 
     def update(self):
+        # update all holdings and overall portfolio
         for holding in self.holdings:
             holding.update()
         self.update_market_value()
@@ -64,16 +69,16 @@ class Holding(db.Model):
     # model for holdings associated with portfolios
     __tablename__ = 'holdings'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    symbol = db.Column(db.String(6))
-    shares = db.Column(db.Integer)
-    market_value = db.Column(db.Float)
-    purch_date = db.Column(db.Date)
-    purch_price = db.Column(db.Float)
-    last_price = db.Column(db.Float)
-    total_profit = db.Column(db.Float)
-    profit_percent = db.Column(db.Float)
-    portfolio_percent = db.Column(db.Float)
-    last_updated = db.Column(db.String)
+    symbol = db.Column(db.String(6))            # stock symbol
+    shares = db.Column(db.Integer)              # number of shares held
+    market_value = db.Column(db.Float)          # market value of holding
+    purch_date = db.Column(db.Date)             # date holding was purchased
+    purch_price = db.Column(db.Float)           # share price on purchase date
+    last_price = db.Column(db.Float)            # most recent price from pandas_dataframe module
+    total_profit = db.Column(db.Float)          # total profits of holding ($)
+    profit_percent = db.Column(db.Float)        # percent profit based on initial amount invested
+    portfolio_percent = db.Column(db.Float)     # percent holding makes up of overall portfolio
+    last_updated = db.Column(db.String)         # date holding's last_price attribute was updated
     portfolio_id = db.Column(db.Integer, db.ForeignKey('portfolios.id'))
 
     def __init__(self, symbol, shares, purch_date, purch_price, portfolio_id):
@@ -89,6 +94,8 @@ class Holding(db.Model):
         return '<Name %r>' % self.symbol
 
     def update_last_price(self):
+        # update last_price from pandas_datareader module
+        # only if last_updated doesn't match today
         import datetime as dt
         if not self.last_updated == str(dt.date.today()):
             from pandas_datareader import data as web
@@ -97,15 +104,18 @@ class Holding(db.Model):
         db.session.add(self)
 
     def update_market_value(self):
+        # update market_value attribute
         self.market_value = self.shares * self.last_price
         db.session.add(self)
 
     def update_profit(self):
+        # update profit attributes
         self.total_profit = round(self.shares * (self.last_price - self.purch_price), 2)
         self.profit_percent = round(self.last_price / self.purch_price - 1, 4)
         db.session.add(self)
 
     def update_portfolio_percentage(self):
+        # update percentage that this holding makes up of portfolio
         self.portfolio_percent = round(self.market_value / Portfolio.query.filter_by(id=self.portfolio_id).first().market_value,4)
         db.session.add(self)
 
